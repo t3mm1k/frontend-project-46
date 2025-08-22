a1c3ec3242ea9e80e618c8a8a3eebd6832c44b89
t3mm1k <146156660+t3mm1k@users.noreply.github.com>
2025-08-22 23:38:52 +0700

    fix: lint

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import parseFile from './parsers.js';
-import buildDiff from './diffBuilder.js';
-import formatStylish from './formatters/stylish.js';
+import parseFile from './parsers.js'
+import buildDiff from './diffBuilder.js'
+import formatStylish from './formatters/stylish.js'
 
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
-  const data1 = parseFile(filepath1);
-  const data2 = parseFile(filepath2);
-  const diff = buildDiff(data1, data2);
-  
-  if (format === 'stylish') {
-    return formatStylish(diff);
-  }
-  
-  throw new Error(`Unknown format: ${format}`);
-};
+  const data1 = parseFile(filepath1)
+  const data2 = parseFile(filepath2)
+  const diff = buildDiff(data1, data2)
 
-export default genDiff;
+  if (format === 'stylish') {
+    return formatStylish(diff)
+  }
+
+  throw new Error(`Unknown format: ${format}`)
+}
+
+export default genDiff
