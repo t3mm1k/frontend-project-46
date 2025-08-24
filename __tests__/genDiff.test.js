ac972f0c9dd909cf7ecd408634b6ec39de90a88e
t3mm1k <146156660+t3mm1k@users.noreply.github.com>
2025-08-25 01:17:39 +0700

    feat: move test to root

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -2,7 +2,7 @@ import { readFileSync } from 'fs'
 import { fileURLToPath } from 'url'
 import { dirname, join } from 'path'
 import path from 'node:path'
-import genDiff from '../index.js'
+import genDiff from '../src/index.js'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
